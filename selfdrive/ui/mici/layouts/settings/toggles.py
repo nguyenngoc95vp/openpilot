4fcee92ed4a33f3b1ff8ef08a79e0153d8be76d2
nguyenngoc95vp <nguyenngoc95vp@gmail.com>
2026-09-12 11:00:32 +0700

    C4 UI: add TI RI radar emulation toggles

--- selfdrive/ui/mici/layouts/settings/toggles.py
+++ selfdrive/ui/mici/layouts/settings/toggles.py
@@ -28,6 +28,12 @@ class TogglesLayoutMici(NavScroller):
     record_front = BigParamControl("record & upload driver camera", "RecordFront", toggle_callback=restart_needed_callback)
     record_mic = BigParamControl("record & upload mic audio", "RecordAudio", toggle_callback=restart_needed_callback)
     enable_openpilot = BigParamControl("enable openpilot", "OpenpilotEnabledToggle", toggle_callback=restart_needed_callback)
+    enable_torque_interceptor = BigParamControl("torque interceptor", "TorqueInterceptorEnabled", toggle_callback=restart_needed_callback)
+    enable_radar_emulation = BigParamControl("radar emulation", "RadarEmulationEnabled", toggle_callback=restart_needed_callback)
+    enable_radar_interceptor = BigParamControl("radar interceptor", "RadarInterceptorEnabled", toggle_callback=restart_needed_callback)
+    enable_no_mrcc = BigParamControl("no stock MRCC", "NoMRCC", toggle_callback=restart_needed_callback)
+    enable_no_fsc = BigParamControl("no stock FSC", "NoFSC", toggle_callback=restart_needed_callback)
+    enable_manual_transmission = BigParamControl("manual transmission", "ManualTransmission", toggle_callback=restart_needed_callback)
 
     self._scroller.add_widgets([
       self._personality_toggle,
@@ -40,6 +46,12 @@ class TogglesLayoutMici(NavScroller):
       record_front,
       record_mic,
       enable_openpilot,
+      enable_torque_interceptor,
+      enable_radar_emulation,
+      enable_radar_interceptor,
+      enable_no_mrcc,
+      enable_no_fsc,
+      enable_manual_transmission,
     ])
 
     # Toggle lists
@@ -53,6 +65,12 @@ class TogglesLayoutMici(NavScroller):
       ("RecordFront", record_front),
       ("RecordAudio", record_mic),
       ("OpenpilotEnabledToggle", enable_openpilot),
+      ("TorqueInterceptorEnabled", enable_torque_interceptor),
+      ("RadarEmulationEnabled", enable_radar_emulation),
+      ("RadarInterceptorEnabled", enable_radar_interceptor),
+      ("NoMRCC", enable_no_mrcc),
+      ("NoFSC", enable_no_fsc),
+      ("ManualTransmission", enable_manual_transmission),
     )
 
     enable_openpilot.set_enabled(lambda: not ui_state.engaged)
@@ -94,9 +112,9 @@ class TogglesLayoutMici(NavScroller):
 
     # CP gating for experimental mode
     if ui_state.CP is not None:
-      if ui_state.experimental_mode_available:
+      if ui_state.has_longitudinal_control:
         self._experimental_btn.set_visible(True)
-        self._personality_toggle.set_visible(ui_state.has_longitudinal_control)
+        self._personality_toggle.set_visible(True)
       else:
         # no long for now
         self._experimental_btn.set_visible(False)
